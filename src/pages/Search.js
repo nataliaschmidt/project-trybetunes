4e91bb827c790817fa3ec4607b516cb349efe803
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-03-13 18:15:55 -0300

    Requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 
+const MIN_LENGTH = 2;
+
 export default class Search extends Component {
+  state = {
+    search: '',
+  };
+
+  handleChange = ({ target }) => {
+    this.setState({
+      [target.name]: target.value,
+    });
+  };
+
   render() {
+    const { search } = this.state;
+
+    const isFormValid = search.length >= MIN_LENGTH;
+
     return (
       <>
         <Header />
-        <div data-testid="page-search">Search</div>
+        <div data-testid="page-search">
+          <form>
+            <label htmlFor="name">
+              Pesquisar:
+              <input
+                data-testid="search-artist-input"
+                type="text"
+                id="search"
+                name="search"
+                value={ search }
+                onChange={ this.handleChange }
+              />
+            </label>
+            <button
+              data-testid="search-artist-button"
+              type="button"
+              // onClick={ () =>  }
+              disabled={ !isFormValid }
+            >
+              Entrar
+
+            </button>
+          </form>
+
+        </div>
       </>
     );
   }
