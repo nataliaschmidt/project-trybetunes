7ed6915daecec23416a64c51225d962d1210438d
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-03-13 16:38:07 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,74 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
+
+const MIN_LENGTH = 3;
 
 export default class Login extends Component {
+  state = {
+    loginName: '',
+    isLoading: false,
+    redirect: false,
+  };
+
+  fetchCreateUser = async () => {
+    const { loginName } = this.state;
+    this.setState({
+      isLoading: true,
+    });
+    const response = await createUser({ name: loginName });
+    console.log(response);
+    if (response === 'OK') {
+      this.setState({
+        isLoading: false,
+        redirect: true,
+      });
+    }
+  };
+
+  handleChange = ({ target }) => {
+    this.setState({
+      [target.name]: target.value,
+    });
+  };
+
   render() {
+    const { loginName, isLoading, redirect } = this.state;
+    // console.log(this.props);
+
+    const isFormValid = loginName.length >= MIN_LENGTH;
+
+    if (isLoading) return <Loading />;
+
+    if (redirect) return <Redirect to="/search" />;
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        <form>
+
+          <label htmlFor="name">
+            Nome:
+            <input
+              data-testid="login-name-input"
+              type="text"
+              id="name"
+              name="loginName"
+              value={ loginName }
+              onChange={ this.handleChange }
+            />
+          </label>
+
+          <button
+            data-testid="login-submit-button"
+            type="button"
+            onClick={ () => this.fetchCreateUser() }
+            disabled={ !isFormValid }
+          >
+            Entrar
+
+          </button>
+        </form>
+      </div>
     );
   }
 }
